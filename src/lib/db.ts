5a5a4cc4b9af16b29bbc4d8e02a8db5635856a26
Luis Angel Iniguez Hernández <luigi.lahi@gmail.com>
2021-06-18 15:24:03 -0500

    fix: DB configuration modified to allow REDIS_URL environment variable and be able to deploy to heroku

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,10 +1,19 @@
 import Assert from 'assert';
 import Redis from 'redis';
 
-Assert.ok(process.env.REDIS_PORT, 'REDIS_PORT is a required environment variable');
-Assert.ok(process.env.REDIS_URL, 'REDIS_URL is a required environment variable');
-Assert.ok(Number.isInteger(Number.parseInt(process.env.REDIS_PORT)), 'REDIS_PORT value is not an integer');
+const redisOptions: Redis.ClientOpts = { };
 
-const client = Redis.createClient({ port: Number.parseInt(process.env.REDIS_PORT), host: process.env.REDIS_URL });
+if (!process.env.REDIS_URL) {
+  Assert.ok(process.env.REDIS_PORT, 'REDIS_PORT is a required environment variable');
+  Assert.ok(process.env.REDIS_HOST, 'REDIS_HOST is a required environment variable');
+  Assert.ok(Number.isInteger(Number.parseInt(process.env.REDIS_PORT)), 'REDIS_PORT value is not an integer');
+
+  redisOptions.port = Number.parseInt(process.env.REDIS_PORT);
+  redisOptions.host = process.env.REDIS_HOST;
+} else {
+  redisOptions.url = process.env.REDIS_URL;
+}
+
+const client = Redis.createClient(redisOptions);
 
 export default client;
