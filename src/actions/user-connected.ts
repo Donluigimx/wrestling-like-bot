3ed07702aa0a974153a12a0e812b566fff13457a
Luis Angel Iniguez Hernández <luigi.lahi@gmail.com>
2021-06-22 13:27:16 -0500

    perf: Improved how bot handles a new user connected to a voice channel

--- src/actions/user-connected.ts
+++ src/actions/user-connected.ts
@@ -7,20 +7,28 @@ import Database from '../lib/db';
 import { AssignPayload } from '../commands/assign';
 
 async function userConnected(userId: string, serverId: string, channel: VoiceChannel): Promise<void> {
-  const connection: VoiceConnection = await channel.join();
   const hash: string = Crypto.createHash('md5').update(`${userId}${serverId}`).digest('hex');
-  const response: AssignPayload  = await new Promise<AssignPayload>((resolve, reject) => {
-    Database.get(hash, (err, reply) => {
-      if (err) {reject(err);}
-      resolve(JSON.parse(reply));
+  let response: AssignPayload;
+
+  try {
+    response  = await new Promise<AssignPayload>((resolve, reject) => {
+      Database.get(hash, (err, reply) => {
+        if (err) {reject(err);}
+        resolve(JSON.parse(reply));
+      });
     });
-  });
+  } catch {
+    return;
+  }
 
   const info = await Ytdl.getInfo(response.url);
   const audioFormats = Ytdl.filterFormats(info.formats, 'audioonly');
+  const audioStream = Ytdl(response.url, { format: audioFormats[0] });
+
+  const connection: VoiceConnection = await channel.join();
 
   connection.play(
-    Ytdl(response.url, { format: audioFormats[0] }),
+    audioStream,
     { seek: Duration(response.starts) / 1000 }
   );
 
